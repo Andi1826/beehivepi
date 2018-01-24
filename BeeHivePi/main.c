db3e22c31e1c0aa9a9920d30d0874e5a6cdc3484
Andi <akaminski015@gmail.com>
2018-01-24 15:29:08 +0100

    - added loop that inserts 11 measurements separated by 1 second into the table "MEASURE"

--- BeeHivePi/main.c
+++ BeeHivePi/main.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <math.h>
 #include <sqlite3.h>
+#include <unistd.h>
+
+
+int maxId = 0;
 
 double getInsideTemp() {
 	double dummyTemp;
@@ -34,11 +38,38 @@ double getOutsideTemp() {
 	return dummyTemp;
 }
 
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+	int i;
+	for (i = 0; i < argc; ++i) {
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	printf("\n");
+	return 0;
+}
+
+static int callbackMaxId(void *NotUsed, int argc, char **argv, char **azColName) {
+	long int i;
+	if (argv[0]) {
+		i = strtol(argv[0], NULL, 10);
+	}
+	else {
+		i = 0;
+	}
+	maxId = (int)i;
+	return 0;
+}
+
 int main()
 {
 	sqlite3 *db;
 	char *errMsg = 0;
 	int rc;
+	char *sql, *sql2;
+	char sqlInsert[150];
+	double insideTemp, humidity, dbLevel, weight, outsideTemp;
+	unsigned int microseconds = 1000000;
+	int i = 0;
+
 	srand(time(NULL));
 
 	rc = sqlite3_open("beehive.db", &db);
@@ -49,6 +80,56 @@ int main()
 		fprintf(stderr, "Opened database succesfully\n");
 	}
 	
+	sql = "CREATE TABLE IF NOT EXISTS MEASURE(" \
+		  "ID INTEGER PRIMARY KEY AUTOINCREMENT," \
+		  "CURRENT_TIME DATETIME," \
+		  "INSIDE_TEMP NUMERIC," \
+		  "HUMIDITY NUMERIC," \
+		  "DB_LEVEL NUMERIC," \
+		  "WEIGHT NUMERIC," \
+		  "OUTSIDE_TEMP NUMERIC);";
+
+	rc = sqlite3_exec(db, sql, callback, 0, &errMsg);
+
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", errMsg);
+		sqlite3_free(errMsg);
+	}
+	else {
+		fprintf(stdout, "Table created successfully\n");
+	}
+
+	sql = "INSERT INTO MEASURE (ID, CURRENT_TIME,INSIDE_TEMP,HUMIDITY,DB_LEVEL,WEIGHT,OUTSIDE_TEMP) " \
+		  "VALUES (%d, DATETIME('now'), %.1f, %.1f, %.1f, %.1f, %.1f);";
+
+	//printf("Press escape to end measurments\n");
+
+	sql2 = "SELECT MAX(ID) FROM MEASURE;";
+
+	rc = sqlite3_exec(db, sql2, callbackMaxId, 0, &errMsg);
+
+	i = 0;
+	while (i <= 10) {
+		insideTemp = getInsideTemp();
+		humidity = getHumidity();
+		dbLevel = getDbLevel();
+		weight = getWeight();
+		outsideTemp = getOutsideTemp();
+
+		sprintf(sqlInsert, sql, maxId+i+1, insideTemp, humidity, dbLevel, weight, outsideTemp);
+		printf("%s\n", sqlInsert);
+		rc = sqlite3_exec(db, sqlInsert, callback, 0, &errMsg);
+
+		if (rc != SQLITE_OK) {
+			fprintf(stderr, "SQL error: %s\n", errMsg);
+			sqlite3_free(errMsg);
+		}
+		else {
+			fprintf(stdout, "Records created successfully\n");
+		}
+		usleep(microseconds);
+		i++;
+	}
 
 	sqlite3_close(db);
     return 0;
